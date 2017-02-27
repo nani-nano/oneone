671f402a27b37ea8b2d9ba2cc42e643cd85cd146
nani-nano <nani-nano@inbox.ru>
2017-02-27 17:52:19 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 
 # update apt-get
 sudo apt-get update
-sudo apt-get install ruby -y
-sudo apt-get install nmap -y
-sudo apt-get install python -y
-sudo apt-get install ssmtp -y
-sudo dpkg-query -l | grep ssmtp | wc -l
+#Check the package and install-----------------
+echo "Check the package and install"
 while read -r line; do
    sudo apt-get install $line -y
    chk=$(sudo dpkg-query -l | grep $line | wc -l)
