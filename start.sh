f15ccbaba7455b8677414bb1185e85d6354bd3e1
nani-nano <nani-nano@inbox.ru>
2017-02-27 18:23:27 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 #Check the package and install-----------------
 echo "Check the package and install"
 while read -r line; do
+   echo "Check the package [+ $line +] and install"
    sudo apt-get install $line -y
    chk=$(sudo dpkg-query -l | grep $line | wc -l)
    echo $chk  
