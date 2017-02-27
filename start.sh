a8d730c148f59e63db1b33f825caf6d872bfd905
nani-nano <nani-nano@inbox.ru>
2017-02-27 17:48:36 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,3 +4,10 @@ sudo apt-get update
 sudo apt-get install ruby -y
 sudo apt-get install nmap -y
 sudo apt-get install python -y
+sudo apt-get install ssmtp -y
+sudo dpkg-query -l | grep ssmtp | wc -l
+while read -r line; do
+   sudo apt-get install $line -y
+   chk=$(sudo dpkg-query -l | grep $line | wc -l)
+   echo $chk  
+done < pack
